31579f33a25ea5bb1da03b9ebf639c424033f66b
namdng09 <namduong090104@gmail.com>
2025-07-14 13:33:46 +0700

    feat: define the layout

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        <Text>Edit <Text style={{ fontFamily: "monospace" }}>app/index.tsx</Text> to edit this screen.</Text>
+      </Text>
+
+    <Link href="/(auth)/signup">Signup</Link>
+    <Link href="/(auth)">Login</Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "blue",
+  },
+});
